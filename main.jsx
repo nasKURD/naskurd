2acb528b433b98ca2f5634b01fc254723ac2ca69
nasKURD <kurdnasname@gmail.com>
2026-04-07 17:52:43 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import KurdiWallet from './KurdiWallet.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <KurdiWallet />
+  </StrictMode>,
+)
