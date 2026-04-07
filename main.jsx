181931abc83c70415f447c9eb0cb311178de8d11
nasKURD <kurdnasname@gmail.com>
2026-04-07 18:08:53 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import KurdiWallet from '../KurdiWallet.jsx' 
+import KurdiWallet from './KurdiWallet.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
